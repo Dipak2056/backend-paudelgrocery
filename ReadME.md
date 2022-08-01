2c3273c6ce88b5f10f3f8947e1e4b957482a90ac
Dipak 2056 <paudelsantosh508@gmail.com>
2022-08-01 10:08:16 +1000

    romiving node_modules

---
+++ ReadME.md
@@ -0,0 +1,5 @@
+# initialized the app
+
+1. install all the dependencies i could think of to use in the app.
+2. created git repo
+3. created Readme file
---
+++ server.js
@@ -0,0 +1,9 @@
+import express from "express";
+const app = express();
+//declaring port number to use
+const PORT =
+  //middle wares will go here
+
+  app.listen(8000, () => {
+    console.log("app is running in port 8000");
+  });
