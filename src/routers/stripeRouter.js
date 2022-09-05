ac7fdd253863d8afa8cc8bb75cc0c1aee6417f08
Dipak 2056 <paudelsantosh508@gmail.com>
2022-09-05 17:50:08 +1000

    backend done for stripe integration

--- src/routers/stripeRouter.js
+++ src/routers/stripeRouter.js
@@ -1,9 +1,10 @@
+import "dotenv/config";
 import express from "express";
-import {
-  getAllProducts,
-  getProductById,
-} from "../models/product-models/products.model.js";
+import { getProductById } from "../models/product-models/products.model.js";
 const router = express.Router();
+//stripe integration
+import Stripe from "stripe";
+const stripe = new Stripe(process.env.STRIPE_PRIVATE_KEY);
 
 router.post("/", async (req, res) => {
   const { items } = req.body;
@@ -15,7 +16,31 @@ router.post("/", async (req, res) => {
     const quantity = items[i].quantity;
     details.push({ price, name, quantity });
   }
+
   //stripe integration part
+  try {
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ["card"],
+      mode: "payment",
+      line_items: details.map((item) => {
+        return {
+          price_data: {
+            currency: "aud",
+            product_data: {
+              name: item.name,
+            },
+            unit_amount: item.price * 100,
+          },
+          quantity: item.quantity,
+        };
+      }),
+      success_url: `${process.env.CLIENT_URL}/success.html`,
+      cancel_url: `${process.env.CLIENT_URL}/cancel.html`,
+    });
+    res.json({ url: session.url });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 
   //   const products = await getAllProducts({ _id });
 
